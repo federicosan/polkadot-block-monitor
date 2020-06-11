13c305e71f2a065e1c4e92b4bcd4f607f26d277a
Federico Sananes <federico.sananes@gmail.com>
2020-06-11 16:44:25 -0300

    block number box

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 const Canvas = require('drawille');
 const blessed = require('neo-blessed');
+const contrib = require('./contribdemo');
 const os = require('os');
 const glob = require('glob');
 const path = require('path');
@@ -14,13 +15,13 @@ const  DOT_DECIMAL_PLACES = 1000000000000;
 const { ApiPromise, WsProvider} = require('@polkadot/api');
 const web3Provider = 'wss://cc3-5.kusama.network/';
 
-const drawHeader = (screen) => {
+const drawHeader = (screen, info) => {
 
   let headerText;
   let headerTextNoTags;
 
-  headerText = ` {bold}Polkadot Block Monitor{/bold}{white-fg} for ${os.hostname()}`;
-  headerTextNoTags = `Polkadot for ${os.hostname()} `;
+  headerText = ` {bold}Polkadot Block Monitor{/bold}{white-fg} on ${info.chain}`;
+  headerTextNoTags = `Polkadot Block Monitor on ${info.chain} `;
 
   const header = blessed.text({
     top: 'top',
@@ -37,21 +38,31 @@ const drawHeader = (screen) => {
     width: 9,
     height: '1',
     align: 'right',
-    content: '',
+    content:'' ,
     tags: true
   });
-  const loadAverage = blessed.text({
+  const nodeName = blessed.text({
     top: 'top',
     height: '1',
     align: 'center',
-    content: '',
+    content: `| node ${info.nodeName}`,
     tags: true,
-    left: Math.floor(program.cols / 2 - (28 / 2))
+    left: Math.floor(program.cols / 2 - (50 / 2))
+  });
+
+  const nodeVersion = blessed.text({
+    top: 'top',
+    height: '1',
+    align: 'center',
+    content: `| node version ${info.nodeVersion}`,
+    tags: true,
+    left: Math.floor(program.cols / 2 + 1 )
   });
 
   screen.append(header);
   screen.append(date);
-  screen.append(loadAverage);
+  screen.append(nodeName);
+  screen.append(nodeVersion);
 
   const zeroPad = input => (`0${input}`).slice(-2);
 
@@ -61,16 +72,11 @@ const drawHeader = (screen) => {
     screen.render();
   } 
 
-  const updateLoadAverage = () => {
-    const avg = os.loadavg();
-    loadAverage.setContent(`Load Average: ${avg[0].toFixed(2)} ${avg[1].toFixed(2)} ${avg[2].toFixed(2)}`);
-    screen.render();
-  }
-
+  
   updateTime();
-  updateLoadAverage();
+  
   setInterval(updateTime, 1000);
-  setInterval(updateLoadAverage, 1000);
+  
 }
 
 
@@ -87,10 +93,14 @@ try {
         api.rpc.system.version()
       ]);
 
-    const existentialDesposit = theconsts.balances.existentialDeposit.toNumber();
-
-    console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
-    console.log(`The existential deposit for chain is ${existentialDesposit / DOT_DECIMAL_PLACES} KSM.`);
+    const existentialDesposit = theconsts.balances.existentialDeposit.toNumber() / DOT_DECIMAL_PLACES;
+  
+    return {
+      chain,
+      nodeName,
+      nodeVersion,
+      existentialDesposit
+    };
 
 } catch(e) {
     console.log('Some error with rpc system', e);
@@ -118,12 +128,8 @@ async function main(){
 
 
 const charts = [];
-let loadedTheme;  
-/* let screen; */
 
-/* // Create a screen object.
-screen = blessed.screen();
-screen.title = 'Polkadot Commander'; */
+let screen;
 
   /**
    * This is the number of data points drawn
@@ -142,17 +148,10 @@ screen.title = 'Polkadot Commander'; */
     }
   };
 
-// style themes 
-const files = glob.sync(path.join(__dirname, 'themes', '*.json'));
-for (var i = 0; i < files.length; i++) {
-  let themeName = files[i].replace(path.join(__dirname, 'themes') + path.sep, '').replace('.json', '');
-  themes += `${themeName}|`;
-}
-themes = themes.slice(0, -1);
+ 
 
 // Set up the commander instance and add the required options
 cli
-  .option('-t, --theme  [name]', `set the Polkadot Commander theme [${themes}]`, 'parallax')
   .option('--no-mouse', 'Disables mouse interactivity')
   .option('--quit-after [seconds]', 'Quits Polkadot Commander after interval', '0')
   .option('--update-interval [milliseconds]', 'Interval between updates', '300')
@@ -160,64 +159,95 @@ cli
   .parse(process.argv);
 
 
-// Configure 'q', esc, Ctrl+C for quit
-  let lastKey = '';
-/*   screen.on('keypress', (ch, key) => {
-    if (key === 'up' || key === 'down' || key === 'k' || key === 'j') {
-      // Disable table updates for half a second
-      disableTableUpdate = true;
-      clearTimeout(disableTableUpdateTimeout)
-      disableTableUpdateTimeout = setTimeout(() => {
-        disableTableUpdate = false;
-      }, 1000);
-    }
+  // Create a screen object.
+screen = blessed.screen();
+screen.title = 'Polkadot Commander';
 
-    if (
+  
+const api = await  getApi(getProvider(web3Provider));
 
-      (
-        key.name === 'q' ||
-        key.name === 'escape' ||
-        (key.name === 'c' && key.ctrl === true)
-      )
-    ) {
-      return process.exit(0);
-    }
+const info = await chainInfo(api);
 
-    lastKey = key.name;
-   
-  }); */
 
-//drawHeader(screen);
 
-const api = await getApi(getProvider(web3Provider));
 
-chainInfo(api);
 
+drawHeader(screen, info);
+
+
+let blocks = blessed.box({
+  top: 1,
+  left: 'left',
+  width: '100%',
+  height: '50%',
+  content: '',
+  fg: '#ebdbb2',
+  tags: true,
+  border:{
+    'type': 'line',
+    'fg': '#ebdbb2'
+  },
+  autoPadding: true,
+});
+
+screen.append(blocks);
+
+screen.render();
 
 let count = 0;
 
 // Subscribe to the new headers on-chain. The callback is fired when new headers
 // are found, the call itself returns a promise with a subscription that can be
 // used to unsubscribe from the newHead subscription
-let blockHash;
-let signedBlock;
+let blockHash = {};
+let signedBlock = {};
+let newBlock = 0;
+let newBlockHash = '';
+let newBlockParentHash = '';
+let author = '';
 
 const unsubscribe = await api.derive.chain.subscribeNewHeads(async (header) => {
  try {
-    console.log(`Chain is at block #${header.number} : autor is ${header.author}`);
+    blocks.setContent(newBlock);
+    newBlock = `New block #${header.number}`;
+    author = `${header.author}`;
     blockHash = await api.rpc.chain.getBlockHash(header.number);
-    console.log(`Block hash is ${blockHash}`);
+    newBlockHash = `${blockHash}`;
     signedBlock = await api.rpc.chain.getBlock(blockHash);
-    console.log(`Block's parent hash is ${signedBlock.block.header.parentHash}`);
+    newBlockParentHash = `${signedBlock.block.header.parentHash}`;
  
-  if (++count === 256) {
-    unsubscribe();
-    process.exit(0);
-  }
 } catch(e){
     console.log(e);
 }
 
+});
+
+
+// Configure 'q', esc, Ctrl+C for quit
+let lastKey = '';
+screen.on('keypress', (ch, key) => {
+  if (key === 'up' || key === 'down' || key === 'k' || key === 'j') {
+    // Disable table updates for half a second
+    disableTableUpdate = true;
+    clearTimeout(disableTableUpdateTimeout)
+    disableTableUpdateTimeout = setTimeout(() => {
+      disableTableUpdate = false;
+    }, 1000);
+  }
+
+  if (
+
+    (
+      key.name === 'q' ||
+      key.name === 'escape' ||
+      (key.name === 'c' && key.ctrl === true)
+    )
+  ) {
+    return process.exit(0);
+  }
+
+  lastKey = key.name;
+ 
 });
 
 
